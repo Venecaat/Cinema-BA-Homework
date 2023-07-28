af492c771a8ecf64cc5d85ed7da2d4799875d0f6
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-07-28 10:16:51 +0200

    Remove bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  css: [
-    "~/node_modules/bootstrap/dist/css/bootstrap.min.css"
-  ],
-  plugins: [
-    { src: "~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js", mode: "client" }
-  ],
+  devtools: { enabled: true }
 })
