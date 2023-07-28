94e94a6f2ecbe4678cab376f83f7beb7caadf724
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-07-28 16:04:12 +0200

    Add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+  <!-- Navbar -->
+  <nav class="border-gray-200 dark:bg-gray-900 bg-blue-400">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <NuxtLink to="/" class="self-center text-2xl font-semibold whitespace-nowrap text-cyan-50 hover:text-red-700">Cinema BA</NuxtLink>
+      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+        <ul class="font-medium flex flex-row">
+          <li>
+            <NuxtLink href="/" class="block py-2 pl-3 pr-4 text-cyan-50 hover:text-red-700">Főoldal</NuxtLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page content -->
+  <div class="max-w-screen-xl shadow-2xl rounded-lg mx-auto p-4 mt-4">
+    <slot />
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
