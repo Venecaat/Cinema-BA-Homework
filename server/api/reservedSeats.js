eb6c61960853400d8ac91291802260f6755b1138
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-07-29 21:20:33 +0200

    Add reservedSeats api endpoint

---
+++ server/api/reservedSeats.js
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+export default defineEventHandler((event) => {
+    const { roomName } = getQuery(event);
+    const roomId = prisma.room.findUnique({
+        where: {
+            name: roomName
+        }
+    });
+
+    return prisma.ReservedSeat.findMany({
+        select: {
+            seat_id: true,
+            seat: {
+                select: {
+                    row_number: true,
+                    seat_number: true
+                }
+            }
+        }
+    });
+})
