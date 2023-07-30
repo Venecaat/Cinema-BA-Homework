fc7fb8f711978fd70653b4c2168411d9c1a10fbc
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-07-30 19:20:45 +0200

    Add pinia

---
+++ store/seats.ts
@@ -0,0 +1,19 @@
+import { defineStore } from "pinia";
+
+export const useSeatsStore = defineStore("seats-list", {
+    state: () => ({ seatsList: [] as Seat[] }),
+    actions: {
+        addSeat(value: Seat) {
+            this.seatsList.push(value);
+        }
+    },
+    getters: {
+        getList: state => state.seatsList,
+    },
+})
+
+interface Seat {
+    seat_id: number,
+    row_number: number,
+    seat_number: number
+}
