7b9a00640c5d6a5cdb75982f289ff053b39b5c47
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-07-30 20:38:09 +0200

    Add foglalasKesz.vue to pages

---
+++ pages/foglalasKesz.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="text-center">
+    <h1 class="text-4xl mb-8 font-semibold">A foglalás sikeres volt!</h1>
+    <NuxtLink to="/">
+      <button type="button" class="justify-self-start bg-red-600 hover:bg-red-400 py-3 px-10 text-2xl font-semibold rounded-xl">Vissza a főoldalra</button>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
