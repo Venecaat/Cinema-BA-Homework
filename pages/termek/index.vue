1fbb5ae6be1de7816d64b79a66ef77bbfb9a95a2
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-08-09 15:19:07 +0200

    Add page for rooms

---
+++ pages/termek/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+  const { data } = await useFetch("/api/room/getRooms");
+  const rooms = JSON.parse(JSON.stringify(data.value));
+</script>
+
+<template>
+  <NuxtLink v-for="room in rooms" :to="'/termek/' + room.name.toLowerCase()" class="text-3xl font-semibold mb-4 hover:text-red-700 block max-w-fit">{{ room.name }}</NuxtLink>
+</template>
+
+<style scoped>
+
+</style>
