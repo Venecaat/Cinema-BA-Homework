17fa008a8deb7fce060e1a897e8ac5b93e8b2384
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-08-10 16:22:57 +0200

    Update foglalas page to use BackButton component

--- pages/foglalas.vue
+++ pages/foglalas.vue
@@ -46,15 +46,17 @@
 </script>
 
 <template>
-  <h1 class="text-3xl mb-4">Foglalni kívánt helyek:</h1>
-  <h4 v-for="seat in seatStore.getList" class="ml-3 font-semibold text-xl">Sor: {{ seat.row_number }}, Szék: {{ seat.seat_number }}</h4>
+  <div class="flex justify-between">
+    <div>
+      <h1 class="text-3xl mb-4">Foglalni kívánt helyek:</h1>
+      <h4 v-for="seat in seatStore.getList" class="ml-3 font-semibold text-xl">Sor: {{ seat.row_number }}, Szék: {{ seat.seat_number }}</h4>
+    </div>
+    <BackButton @click="back"></BackButton>
+  </div>
   <div class="text-center mt-4">
     <h3 class="text-2xl mb-4 font-semibold text-red-600">A foglalás véglegesítéséhez kérjük adja meg E-mail címét!</h3>
     <input id="emailAddress" type="text" placeholder="E-mail" class="mb-6 mx-auto border-2 border-gray-300 text-gray-900 text-lg rounded-lg block p-2.5">
-    <div class="grid grid-cols-3">
-      <button type="button" class="justify-self-start bg-red-600 hover:bg-red-400 py-3 px-10 text-2xl font-semibold rounded-xl" @click="back">Vissza</button>
-      <button type="button" class="justify-self-center bg-teal-500 hover:bg-teal-300 py-3 px-10 text-2xl font-semibold rounded-xl" @click="reserveSeats">Lefoglalom</button>
-    </div>
+    <button type="button" class="bg-teal-500 hover:bg-teal-300 py-3 px-10 text-2xl font-semibold rounded-xl" @click="reserveSeats">Lefoglalom</button>
   </div>
 </template>
 
